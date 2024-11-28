d807ac1b36b02d70f084dc26efdfad36414da915
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-11-28 13:56:52 -0500

    use subshell  instead of

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 
 git submodule update --init --remote
 
-echo "reattaching submodule HEAD"
-cd static-site-generator || exit
-git switch main
-cd ..
+(  # just discovered subshell
+    echo "reattaching submodule HEAD"
+    cd static-site-generator || exit
+    git switch main
+)
